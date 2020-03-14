f5434576d65e116e76b5ed5dc22427c24e036493
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-03-14 22:54:12 +0800

    finish RandomGenerator.

--- src/RandomGenerator/main.cpp
+++ src/RandomGenerator/main.cpp
@@ -2,19 +2,22 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 
+#include "Generator.h"
+#include "StringEncoder.h"
+
 using namespace std;
 namespace po = boost::program_options;
 
 /**
- * 
+ * To randomly generate chromosome as original species.
  */
 int main(int argc, char** argv) {
     // Declare the supported options.
     po::options_description desc("Allowed options");
     desc.add_options()
             ("help", "produce help message")
-            ("address", po::value<int>()->default_value(10), "length of addressing space")
-            ("gene", po::value<int>(), "how many genes will generated")
+            ("address", po::value<uint8_t>(), "length of addressing space")
+            ("gene", po::value<uint32_t>(), "how many genes will generated")
             ;
 
     po::variables_map vm;
@@ -25,13 +28,26 @@ int main(int argc, char** argv) {
         cout << desc << "\n";
         return 1;
     }
-
-    if (vm.count("address")) {
-        cout << "Compression level was set to "
-                << vm["address"].as<int>() << ".\n";
-    } else {
-        cout << "Compression level was not set.\n";
+    if (!vm.count("address")) {
+        cout << "Length of address was not set.\n";
+        return 1;
     }
+
+    if (!vm.count("gene")) {
+        cout << "Amount of genes was not set.\n";
+        return 1;
+    }
+
+    try {
+        Generator generator(vm["address"].as< uint8_t>(), vm["gene"].as<uint32_t>());
+        StringEncoder encoder;
+        string result = encoder.encode(generator.getChromosome());
+        cout << result;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
+
     return 0;
 }
 
