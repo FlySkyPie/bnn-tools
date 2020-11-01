76a1e80c5a7be9359a5f6829e44c45d09c29e351
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2020-11-01 10:28:40 +0800

    Add stdin mode to the decoder.

--- src/Decoder/main.cpp
+++ src/Decoder/main.cpp
@@ -14,50 +14,65 @@ namespace po = boost::program_options;
 /**
  * To randomly generate chromosome as original species.
  */
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     // Declare the supported options.
     po::options_description desc("Allowed options");
-    desc.add_options()
-            ("help", "produce help message")
-            ("input-file", po::value<string>(), "the chromosome file")
-            ;
+    desc.add_options()("help", "produce help message")("input-file", po::value<string>(), "the chromosome file");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
 
-    if (vm.count("help")) {
+    if (vm.count("help"))
+    {
         cout << desc << "\n";
         return 1;
     }
-    if (!vm.count("input-file")) {
+    
+    /*if (!vm.count("input-file"))
+    {
         cout << "Input file was not set.\n";
         return 1;
-    }
+    }/**/
 
-    try {
-        //read file
-        string filePath = vm["input-file"].as<string>();
-        ifstream file;
-        file.open(filePath);
-        string chromosomString((istreambuf_iterator<char>(file)),
-                istreambuf_iterator<char>());
-        file.close();
+    try
+    {
+        string chromosomString;
+        if (vm.count("input-file"))
+        {
+            //read file
+            string filePath = vm["input-file"].as<string>();
+            ifstream file;
+            file.open(filePath);
+            string chromosomString((istreambuf_iterator<char>(file)),
+                                   istreambuf_iterator<char>());
+            file.close();
+        }
+        else
+        {
+            char c;
+            while (std::cin.get(c))
+            {
+                chromosomString.push_back(c);
+            }
+        }
 
         //decode to chromosome from string
         StringDecoder stringDecoder;
         Decoder decoder;
-        Chromosome * chromosome = stringDecoder.decode(chromosomString);
+        Chromosome *chromosome = stringDecoder.decode(chromosomString);
 
         //decode to json from chromosome
         string result = decoder.convertToJson(chromosome);
         cout << result;
         delete chromosome;
-    } catch (const std::exception& e) {
+    }
+    catch (const std::exception &e)
+    {
         std::cerr << e.what() << std::endl;
         return 1;
     }
 
     return 0;
 }
-
