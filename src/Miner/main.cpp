335837ec91682091c1811fe3c35d462bc5e9058d
FlySkyPie(Wei Ji) <c445dj544@gmail.com>
2021-05-27 07:35:34 +0800

    Add header.

--- src/Miner/main.cpp
+++ src/Miner/main.cpp
@@ -9,7 +9,7 @@ namespace po = boost::program_options;
 
 typedef unsigned char uchar;
 
-unsigned char* createMask(int geneAddressing) {
+uchar* createMask(int geneAddressing) {
     uint* mask = new uint[4];
     int32_t shift[] = {
         min(max(8 - geneAddressing,0), 8),
@@ -31,41 +31,44 @@ unsigned char* createMask(int geneAddressing) {
     return charMask;
 }
 
-unsigned char* andLink(unsigned char* link, uchar* mask) {
+uchar* maskCharArray(uchar* array, uchar* mask) {
     uchar* result = new uchar[4];
-    result[0] = 0xff & link[0] & mask[0]; //LSB in 0b00000001
-    result[1] = 0xff & link[1] & mask[1];
-    result[2] = 0xff & link[2] & mask[2];
-    result[3] = 0xff & link[3] & mask[3]; //MSB in 0b10000000
+    result[0] = 0xff & array[0] & mask[0]; //LSB in 0b00000001
+    result[1] = 0xff & array[1] & mask[1];
+    result[2] = 0xff & array[2] & mask[2];
+    result[3] = 0xff & array[3] & mask[3]; //MSB in 0b10000000
 
     return result;
 }
 
-uint32_t parseLink(unsigned char* link) {
+uint32_t parseLink(uchar* array) {
     uint32_t value = 0x0000 +
-        (uint32_t)link[0] * 0x1 +
-        (uint32_t)link[1] * 0x100 +
-        (uint32_t)link[2] * 0x10000 +
-        (uint32_t)link[3] * 0x1000000;
-    bitset<8> a(link[0]);
-    bitset<8> b(link[1]);
-    bitset<8> c(link[2]);
-    bitset<8> d(link[3]);
+        (uint32_t)array[0] * 0x1 +
+        (uint32_t)array[1] * 0x100 +
+        (uint32_t)array[2] * 0x10000 +
+        (uint32_t)array[3] * 0x1000000;
 
     return value;
 }
 
-
-string  decode(unsigned char* rand1, uchar* rand2, uchar* randWeight, uint32_t geneAddressing) {
-    uchar* mask = createMask(geneAddressing);
-    uchar* link1 = andLink(rand1, mask);
-    uchar* link2 = andLink(rand2, mask);
+string createGeneJsonl(uchar* rand1, uchar* rand2, uchar* randWeight, uint32_t geneScale) {
+    uchar* mask = createMask(geneScale);
+    uchar* link1 = maskCharArray(rand1, mask);
+    uchar* link2 = maskCharArray(rand2, mask);
     bool weight = randWeight[0] & 0x01;
     string gene;
     stringstream ss;
-    ss << "{\"from\":" << parseLink(link1)
+    ss << "{\"type\":\"link\""
+        << ",\"from\":" << parseLink(link1)
         << ",\"to\":" << parseLink(link2)
         << ",\"weight\":" << (weight ? "1" : "0") << "}\n";
+    return ss.str();
+}
+
+string createHeader(uint32_t geneScale) {
+    stringstream ss;
+    ss << "{\"type\":\"head\""
+        << ",\"scale\":" << geneScale << "}\n";
     return ss.str();
 }
 
@@ -77,9 +80,8 @@ int main(int argc, char** argv) {
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
-        ("address", po::value<unsigned>(), "length of addressing space")
-        ("gene", po::value<uint32_t>(), "how many genes will generated")
-        ;
+        ("scale", po::value<uint32_t>(), "length of addressing space")
+        ("size", po::value<uint32_t>(), "how many genes will generated");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -89,23 +91,25 @@ int main(int argc, char** argv) {
         cerr << desc << "\n";
         return 1;
     }
-    if (!vm.count("address")) {
+    if (!vm.count("scale")) {
         cerr << "Length of address was not set.\n";
         return 1;
     }
 
-    if (!vm.count("gene")) {
+    if (!vm.count("size")) {
         cerr << "Amount of genes was not set.\n";
         return 1;
     }
 
     uchar str[9];
-    uint32_t geneAddressing = vm["address"].as<uint32_t>();
-    uint32_t chromosomeSize = vm["gene"].as<uint32_t>();
+    uint32_t geneScale = vm["scale"].as<uint32_t>();
+    uint32_t chromosomeSize = vm["size"].as<uint32_t>();
     uint32_t count = 0;
+
+    std::cout << createHeader(geneScale);
     while (1) {
         for (int i = 0;i < 9;i++) {
-            str[i] = static_cast<unsigned char>(std::cin.get());
+            str[i] = static_cast<uchar>(std::cin.get());
         }
 
         uchar link1[] = {
@@ -118,8 +122,7 @@ int main(int argc, char** argv) {
             str[8]
         };
 
-        std::cout << decode(&link1[0], &link2[0], &weight[0], geneAddressing);
-        //std::cerr << "generate";
+        std::cout << createGeneJsonl(&link1[0], &link2[0], &weight[0], geneScale);
         count += 1;
 
         if (count == chromosomeSize) {
